9ad631e5c47b08d30a2cdf65bb30ae02010ed2d0
Diego Peralta <diego.peralta.dev@gmail.com>
2012-07-07 22:40:07 -0300

    Ahora la ruta default es el dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Nixel::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
+  root :to => 'admin/dashboard#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
